6c59d806b1da3945460ede2468137fa36d6ade5c
jessestrode <jessedans91@gmail.com>
2015-10-01 13:46:48 -0400

    Initial Commit

---
+++ mipsConverter/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mipsConverter
+//
+//  Created by Jesse Strode on 10/1/15.
+//  Copyright © 2015 Jesse Strode. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
